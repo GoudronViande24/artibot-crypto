a2e9d1a7172b952ea6083d54e7628b6a1b498b8a
Thomas Fournier <thomas@artivain.com>
2022-05-10 21:50:04 -0400

    update to artibot v3

--- index.js
+++ index.js
@@ -1,21 +1,96 @@
-// Manifest file for crypto module
+import Artibot, { Module, SlashCommand } from "artibot";
+import Localizer from "artibot-localizer";
 
-module.exports = {
-	manifest: {
+import path from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from 'module';
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import axios from "axios";
 
-		manifestVersion: 1,
-		moduleVersion: "1.0.1",
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const require = createRequire(import.meta.url);
+const { version } = require('./package.json');
+
+/**
+ * Cryptocurrencies market value
+ * Uses Coinbase's API to get current values
+ * @author GoudronViande24
+ * @license MIT
+ * @param {Artibot} artibot
+ */
+export default ({ config: { lang } }) => {
+	localizer.setLocale(lang);
+
+	return new Module({
+		id: "crypto",
 		name: "Crypto",
-		supportedLocales: [
-			"en",
-			"fr"
+		version,
+		repo: "GoudronViande24/artibot-crypto",
+		langs: [
+			"fr",
+			"en"
 		],
 		parts: [
-			{
+			new SlashCommand({
 				id: "crypto",
-				type: "slashcommand",
-				path: "crypto.js"
-			}
+				data: new SlashCommandBuilder()
+					.setName("crypto")
+					.setDescription(localizer._("Get the current value of a cryptocurrency."))
+					.addStringOption(option =>
+						option.setName("crypto")
+							.setDescription(localizer._("The cryptocurrency to look for"))
+							.setRequired(true)
+							.addChoice("Bitcoin", "BTC")
+							.addChoice("Ethereum", "ETH")
+					),
+				mainFunction
+			})
 		]
+	});
+}
+
+const localizer = new Localizer({
+	filePath: path.join(__dirname, "locales.json")
+});
+
+/**
+ * Check value of a cryptocurrency
+ * @param {CommandInteraction} interaction 
+ * @param {Artibot} artibot 
+ */
+async function mainFunction(interaction, { modules, version, config, createEmbed }) {
+	let currencies;
+	
+	if (config.crypto && config.crypto.currencies) {
+		currencies = config.crypto.currencies;
+	} else {
+		currencies = ["CAD", "EUR", "USD"];
 	}
-};
+
+	const crypto = interaction.options.getString("crypto");
+	let costs = "";
+
+	for (const currency of currencies) {
+		let url = `https://api.coinbase.com/v2/prices/${crypto}-${currency}/spot`;
+		const request = await axios(url, {
+			method: "GET",
+			headers: {
+				"User-Agent": `Artibot/${version} artibot-crypto/${modules.find(module => module.id = "crypto").version}`
+			}
+		});
+
+		let { data } = request.data;
+		costs += `**${data.currency}**: ${parseFloat(data.amount).toFixed(2)}\n`;
+	}
+
+	const embed = createEmbed()
+		.setTitle(`${localizer._("Actual value")} - ${crypto}`)
+		.setDescription(costs + "\n`" + localizer._("Data fetched from Coinbase") + "`");
+
+	await interaction.reply({
+		embeds: [embed]
+	});
+}
